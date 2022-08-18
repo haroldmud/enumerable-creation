dffc498d50940fb891c5338b70a1c590e197ef95
haroldmud <haroldmudosa@gmail.com>
2022-08-19 01:04:42 +0200

    Defining list as new array

--- MyList.rb
+++ MyList.rb
@@ -10,3 +10,14 @@ include MyEnumerable
     @list.each(&block)
   end
 end
+
+
+list = new.Mylist(1,2,3,4,5)
+
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+print(list.filter?(&:even?)) 
